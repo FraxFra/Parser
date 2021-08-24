443cd161296418f2c3985328736e50c6c91080eb
Francesco <89411948+FraxFra@users.noreply.github.com>
2021-08-24 20:42:17 +0200

    Create NumberTok.java

---
+++ NumberTok.java
@@ -0,0 +1,13 @@
+public class NumberTok extends Token {
+	public int numero;
+	public NumberTok(int tag, int n)
+  	{
+    		super(tag);
+   		numero = n;
+  	}
+
+  	public String toString()
+  	{
+    		return "<" + tag + ", " + numero + ">";
+  	}
+}
